a1d7a8d78dc553f346b008068ac0bd1396daf86d
mhmmdAkkynk <mhmmd_akkynk@hotmail.com>
2021-05-14 17:18:18 +0300

    Add project files.

---
+++ MyListFunc/MyList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyListFunc
+{
+    class MyList<T>
+    {
+        T[] array;
+        T[] tempArray;
+        public MyList()
+        {
+            array = new T[0]; 
+        }
+
+        public void Add(T value)
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return array.Length; }
+        }
+
+        public T[] GetValue
+        {
+            get { return array; }
+        }
+    }
+}
---
+++ MyListFunc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyListFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myListSample = new List<string>();
+            myListSample.Add("Muhammed");
+            myListSample.Add("Akkaynak");
+            Console.WriteLine("MyListSample Count : " +myListSample.Count);
+
+            foreach (var value in myListSample)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("*-----------*");
+            Console.WriteLine();
+
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Muhammed");
+            myList.Add("Arda");
+            myList.Add("Akkaynak");
+            Console.WriteLine("MyList Count : " + myList.Count);
+
+            foreach (var value in myList.GetValue)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
